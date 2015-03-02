584ab960c2aeb41e838d0a9c09b7bf6d4986de7c
yss44 <nya060@gmail.com>
2015-03-02 23:03:35 +0900

    enable channel search by /youtube/search API

--- lib/yt/collections/channels.rb
+++ lib/yt/collections/channels.rb
@@ -10,21 +10,56 @@ module Yt
     private
 
       def attributes_for_new_item(data)
+        id = use_list_endpoint? ? data["id"] : data["id"]["channelId"]
         super(data).tap do |attributes|
+          attributes[:id]         = id
           attributes[:statistics] = data['statistics']
+          attributes[:snippet]    = data["snippet"]
+          attributes[:status]     = data['status']
         end
       end
 
       # @return [Hash] the parameters to submit to YouTube to list channels.
       # @see https://developers.google.com/youtube/v3/docs/channels/list
       def list_params
-        super.tap{|params| params[:params] = channels_params}
+        super.tap do |params|
+          params[:params] = channels_params
+          params[:path]   = channels_path
+        end
+      end
+
+      def next_page
+        super.tap do |items|
+          add_offset_to(items) if !use_list_endpoint? && @page_token.nil? && channels_params[:order] == 'date'
+        end
+      end
+
+      def add_offset_to(items)
+        if items.count == channels_params[:max_results]
+          last_published = items.last['snippet']['publishedAt']
+          @page_token, @published_before = '', last_published
+        end
       end
 
       def channels_params
-        params = resources_params
-        params.merge! mine: true if @parent
+        params = resources_params.tap do |params|
+          params[:mine]  = true if @parent
+          params[:part]  = "snippet" if !use_list_endpoint?
+          params[:type]  = "channel" if !use_list_endpoint?
+          params[:order] = "date"    if !use_list_endpoint?
+          params[:published_before] = @published_before if @published_before
+
+        end
         apply_where_params! params
+      end
+
+      def channels_path
+        use_list_endpoint? ? "/youtube/v3/channels" : "/youtube/v3/search"
+      end
+
+      def use_list_endpoint?
+        @where_params ||= {}
+        @parent.nil? && (@where_params.keys & [:category_id, :for_username, :id, :managed_by_me]).any?
       end
     end
   end
